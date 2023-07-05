ada8dc71c9b47d96cdef8af4ea0a6d3c0822ab97
vbrianezi <111630170+vbrianezi@users.noreply.github.com>
2023-07-05 13:56:00 -0300

    subindo codigo

---
+++ ControleDeContatos/ControleDeContatos/Controllers/ContatoController.cs
@@ -0,0 +1,110 @@
+﻿using ControleDeContatos.Models;
+using ControleDeContatos.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        private readonly IContatoRepositorio _contatoRepositorio;
+        public ContatoController(IContatoRepositorio contatoRepositorio)
+        {
+            _contatoRepositorio = contatoRepositorio;
+        }
+
+        public IActionResult Index()
+        {
+            List<ContatoModel> contatos = _contatoRepositorio.BuscarTodos();
+            return View(contatos);
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar(int id)
+        {
+            ContatoModel contato = _contatoRepositorio.ListarPorId(id);
+            return View(contato);
+        }
+
+        public IActionResult ApagarConfirmacao(int id)
+        {
+            ContatoModel contato = _contatoRepositorio.ListarPorId(id);
+            return View(contato);
+        }
+
+        [HttpGet]
+        public IActionResult Apagar(int id)
+        {
+            try
+            {
+                bool apagado = _contatoRepositorio.Apagar(id);
+                if (apagado)
+                {
+                    TempData["MensagemSucesso"] = $"Contato apagado com sucesso";
+                }
+                else
+                {
+                    TempData["MensagemErro"] = $"Ops, não conseguimos apagar o contato, tente novamente.";
+                }
+                
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos apagar o contato, tente novamente. Detalhe do erro:{erro.Message}";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // métodos post
+        [HttpPost]
+        public IActionResult Criar(ContatoModel contato)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _contatoRepositorio.Adicionar(contato);
+                    TempData["MensagemSucesso"] = $"Contato {contato.Nome} cadastrado com sucesso";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos cadastrar o contato {contato.Nome}, tente novamente. Detalhe do erro:{erro.Message}";
+                return RedirectToAction("Index");
+            }
+
+            return View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Alterar(ContatoModel contato)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _contatoRepositorio.Atualizar(contato);
+                    TempData["MensagemSucesso"] = $"Contato {contato.Nome} atualizado com sucesso";
+                    return RedirectToAction("Index");
+                }
+            }
+             
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos atualizar o contato {contato.Nome}, tente novamente. Detalhe do erro:{erro.Message}";
+                return RedirectToAction("Index");
+            }
+
+            return View("Editar", contato);
+        }
+
+    }
+}
---
+++ ControleDeContatos/ControleDeContatos/Models/ContatoModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ControleDeContatos.Models
+{
+    public class ContatoModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informe seu nome")]
+        [StringLength(50, ErrorMessage = "O campo Nome permite no máximo 50 caracteres!")]
+        public string Nome { get; set; }
+
+        [StringLength(50)]
+        [Required(ErrorMessage = "Informe o Email")]
+        [EmailAddress(ErrorMessage ="O e-mail informado não é válido")]
+        public string Email{ get; set; }
+
+        [Required(ErrorMessage = "Informe o Telefone")]
+        [Phone(ErrorMessage ="Telefone inválido")]
+        public string Telefone { get; set; }
+    }
+}
